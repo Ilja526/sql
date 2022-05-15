78101cc3a63afa81cd023e0f8a8531a3339a26c2
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-15 12:14:19 +0300

    Add files via upload

---
+++ izglitibas iestade.sql
@@ -0,0 +1,5 @@
+INSERT INTO mācību plāni VALUES("5",5,80,120,"9:matematika","5:programesana specialistes","1:ROBERTS");
+INSERT INTO mācību plāni VALUES("2",6,80,120,"13:latviesu valoda","3:datorsistemas specialitates","4:GUSTAVS");
+INSERT INTO mācību plāni VALUES("3",6,80,53,"12:fizika","4:programesana specialitates","2:OLIVERS");
+INSERT INTO mācību plāni VALUES("4",8,80,53,"11:sports","prograas:datorsistemas specialitates","5:MARKUSS");
+INSERT INTO mācību plāni VALUES("7",8,80,53,"10:anglu valoda","2:programesana specialitates","3:MARKS");
