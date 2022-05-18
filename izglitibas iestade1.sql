737c68def197b81b5539af9cd641ebff985c9596
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-18 13:20:20 +0300

    Delete izglitibas iestade1.sql

--- izglitibas iestade1.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO diploma vērtējumi VALUES(11,7,"1:050604-78932","13:latviesu valoda");
-INSERT INTO diploma vērtējumi VALUES(13,8,"4:090804-51348","9:matematika");
-INSERT INTO diploma vērtējumi VALUES(14,9,"2:071203-34678","11:sports");
-INSERT INTO diploma vērtējumi VALUES(16,7,"3:150402-81634","10:anglu valoda");
-INSERT INTO diploma vērtējumi VALUES(18,9,"5:130804-35126","12:fizika");
