49a41710d67153da9c179b19aed3726acfcf1127
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-09-28 07:57:07 +0300

    Add files via upload

--- Query #3.sql
+++ Query #3.sql
@@ -22,7 +22,7 @@ SELECT
       Number
 FROM skolotaji
 WHERE Surname NOT LIKE 'B%' AND Surname NOT LIKE'%b%' AND Surname NOT LIKE'%b';
-INSERT INTO audzekni VALUES("050407-85642","Ilja","Ivaņenkovs",74893269,NULL,17,"D4-1");
+INSERT INTO audzekni VALUES("050707-85642","Ilja","Ivaņenkovs",74893269,NULL,17,"D4-1");
 SELECT
      PerKods_ID,
      Name,
@@ -33,3 +33,22 @@ SELECT
      FK_GroupName_ID
 FROM audzekni
 WHERE BirthDate IS NULL;
+SELECT
+      BirthDate
+FROM audzekni
+WHERE BirthDate IN('2004-12-31','2004-12-28','2000-12-12','2022-09-13');
+SELECT *
+FROM audzekni
+WHERE
+      MONTH(BirthDate)>"5"
+         AND
+      MONTH(BirthDate)<"9";
+SELECT *
+FROM audzekni
+WHERE MONTH(BirthDate) BETWEEN 1 AND 7;
+SELECT *
+FROM audzekni
+WHERE DAY(BirthDate) BETWEEN 11 AND 31;
+SELECT *
+FROM audzekni
+WHERE NOT(DAY(BirthDate) IN(17,25) OR MONTH(BirthDate) IN(7,8,9))
