4bfb69fcfa25c77db739f3c8c2f13d2c6ba70dc7
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-10-11 17:51:22 +0300

    Add files via upload

--- Query #5.sql
+++ Query #5.sql
@@ -1,17 +1,27 @@
 SELECT
-     COUNT(PerKods_ID) AS "Audzekņa skaitu"
+		COUNT(FK_GroupName_ID) AS GroupName_ID
 FROM audzekni
 WHERE FK_GroupName_ID="D4-1";
 SELECT
-     COUNT(PerKods_ID) AS "Audzekņa skaitu"
+      COUNT(BirthDate) AS Dzimšanas_diena
 FROM audzekni
 WHERE YEAR(BirthDate)=1999 OR YEAR(BirthDate)=2000;
 SELECT
-     AVG(Vertejumi) AS "Videja atzime"
+      AVG(Vertejumi) AS Vidējā_vertejumi
 FROM diploma_vertejumi;
 SELECT
-     AVG(Vertejumi) AS "Videja atzime",
-     MIN(Vertejumi) AS "Minimala atzime",
-     MAX(Vertejumi) AS "Maksimala atzime"
-FROM diploma_vertejumi; 
+      AVG(Vertejumi) AS Vidējā_vertejumi,
+      MIN(Vertejumi) AS Minimālā_vertejumi,
+      MAX(Vertejumi) AS Maksimālā_vertejumi
+FROM diploma_vertejumi
 WHERE FK_PerKods_ID="010205-22708";
+SELECT
+      COUNT(Vertejumi) AS Vertejumi_skaitlis
+FROM diploma_vertejumi
+WHERE Vertejumi<4;
+INSERT INTO diploma_vertejumi VALUES(28,NULL,"010205-22708",3);
+SELECT
+      AVG(Vertejumi) AS Vidējā_vertejumi,
+      MIN(Vertejumi) AS Minimālā_vertejumi,
+      MAX(Vertejumi) AS Maksimālā_vertejumi
+FROM diploma_vertejumi;
